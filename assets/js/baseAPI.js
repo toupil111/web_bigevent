1ea7602685c62ba6fbc8d098b9b2bb35de212884
toupiL <toupil272@gmail.com>
2021-04-04 21:36:59 +0800

    写了user

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,15 +2,17 @@
 //这个函数我们可以拿到他的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
 
 
     options.complete = function(res) {
-        console.log(res);
+        // console.log('执行了 complete 回调：')
+        // console.log(res)
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-
+            // 1. 强制清空 token
+            console.log(1);
             localStorage.removeItem('token')
-
+                // 2. 强制跳转到登录页面
             location.href = '/login.html'
         }
     }
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,56 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6个之间！'
+            }
+        }
+    })
+
+
+    initUserInfo()
+
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                // console.log(res)
+                // 调用 form.val() 快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    $('#btnReset').on('click', function(e) {
+        e.preventDefault();
+        initUserInfo()
+    })
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+
+                window.parent.getUserInfo()
+            }
+
+
+        })
+    })
+})
